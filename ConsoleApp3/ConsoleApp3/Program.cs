43a0c5a5009f5456b08e6a26f8d19ce662f416f8
AlwaysOnCue47 <martin.fobert@gmail.com>
2025-01-13 19:57:25 -0500

    console app 3 - used for quiz

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 17;
+            float num2 = 12.7f;
+
+            float total = (float)num1 + num2;
+
+            Console.WriteLine(total);
+        }
+    }
+}
