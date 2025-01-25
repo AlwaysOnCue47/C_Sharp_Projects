36beea1b6ffca964a286547aa42e01d1e123292d
AlwaysOnCue47 <martin.fobert@gmail.com>
2025-01-25 09:28:34 -0500

    finished mod 2 assignment - exception handling

--- Module2Assignment/Module2Assignment/obj/Debug/netcoreapp3.1/Module2Assignment.AssemblyInfo.cs
+++ Module2Assignment/Module2Assignment/obj/Debug/netcoreapp3.1/Module2Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module2Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fc7938fd94143009f37b2dbb4743c4f0f868c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a958459a9195f81ea9c13a99a05d16d061da1ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module2Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module2Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
