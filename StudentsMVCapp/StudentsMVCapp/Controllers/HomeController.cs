903949d78b4bc78c7bdd5d27a560be32a91e8c34
AlwaysOnCue47 <martin.fobert@gmail.com>
2025-03-05 16:45:17 -0500

    Module 11 MVC Assignment
    Re-doing module 11 and 12 assignments for extra study.

---
+++ StudentsMVCapp/StudentsMVCapp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentsMVCapp.Models;
+
+namespace StudentsMVCapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Instructor(int id)
+        {
+            ViewBag.Id = id;
+
+            Instructor dayTimeInstructor = new Instructor
+            {
+                Id = 1,
+                FirstName = "Jim",
+                LastName = "Kirk"
+            };
+
+            return View(dayTimeInstructor);
+        }
+
+        public ActionResult Instructors()
+        {
+            List<Instructor> instructors = new List<Instructor>
+            {
+                new Instructor
+                {
+                    Id = 1,
+                    FirstName = "William",
+                    LastName = "Riker"
+                },
+                new Instructor
+                {
+                    Id = 2,
+                    FirstName = "Geordie",
+                    LastName = "LaForge"
+                },
+                new Instructor
+                {
+                    Id = 3,
+                    FirstName = "Beverly",
+                    LastName = "Crusher"
+                }
+            };
+            return View(instructors);
+        }
+    }
+}
---
+++ StudentsMVCapp/StudentsMVCapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentsMVCapp.MvcApplication" Language="C#" %>
