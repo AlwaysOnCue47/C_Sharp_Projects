6ba3294e36bfd163cfefe406221861b47bea6850
AlwaysOnCue47 <martin.fobert@gmail.com>
2025-03-16 18:19:04 -0400

    Almost done MVC assignment

---
+++ CarInsuranceApp1/CarInsuranceApp1/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarInsuranceApp1.Models; 
+
+namespace CarInsuranceApp1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private Entities db = new Entities(); // Connects to your database
+
+        public ActionResult Index()
+        {
+            var insurees = db.Table1Insurees.ToList(); // Get all customers
+            foreach (var insuree in insurees)
+            {
+                insuree.Quote = InsureePartial.CalculateQuote(insuree);
+            }
+            return View(insurees);
+        }
+    }
+}
---
+++ CarInsuranceApp1/CarInsuranceApp1/Models/InsureePartial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsuranceApp1.Models
+{
+    public static class InsureePartial
+    {
+        public static decimal CalculateQuote(Table1Insurees insuree)
+        {
+            decimal baseRate = 50m; // Base insurance rate
+
+            int age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+
+            // Add cost based on age
+            if (age <= 18) baseRate += 100;
+            else if (age >= 19 && age <= 25) baseRate += 50;
+            else if (age > 25) baseRate += 25;
+
+            // Add cost for speeding tickets
+            baseRate += insuree.SpeedingTickets * 10;
+          
+            return baseRate;
+        }
+    }
+}
