f4b2db5ebbc61a25e22278151d1074a9ea1e5f34
AlwaysOnCue47 <martin.fobert@gmail.com>
2025-03-19 11:35:15 -0400

    EF Code First Console App - complete

---
+++ EFCodeFirstProject/EFCodeFirstProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EFCodeFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Final Assignment Submission App");
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Demo completed.");
+            Console.ReadLine();
+
+        }
+    }
+}
